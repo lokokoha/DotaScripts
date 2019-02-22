9ce1cd437f30c7ab42e275fa1a3ebe6d3b5061b7
lokokoha <palash.96@mail.ru>
2019-02-22 19:04:08 +0700

    Update Support add check cooldown

--- support.lua
+++ support.lua
@@ -10,7 +10,9 @@ Support.optionWardsIcon = Menu.AddOptionIcon({"Support", "Auto purchase", "Wards
 Support.optionTomeIcon = Menu.AddOptionIcon({"Support", "Auto purchase", "TOME"})
 Support.optionDazzleIcon = Menu.AddOptionIcon({"Support", "Dazzle"}, "panorama/images/heroes/icons/npc_dota_hero_dazzle_png.vtex_c")
 Support.optionEnabledDazzle = Menu.AddOptionBool({"Support", "Dazzle"}, "Enabled", false)
-Support.optionCountEnemyGrave = Menu.AddOptionSlider({"Support","Dazzle"}, "Health", 1, 100, 1)
+Support.optionDazzleGraveIcon = Menu.AddOptionIcon({"Support", "Dazzle", "Grave"}, "panorama/images/spellicons/dazzle/immortal/dazzle_shallow_grave_png.vtex_c")
+Support.optionDazzleWaveIcon = Menu.AddOptionIcon({"Support", "Dazzle", "Wave"}, "panorama/images/spellicons/dazzle/immortal/dazzle_shallow_grave_png.vtex_c")
+Support.optionCountEnemyGrave = Menu.AddOptionSlider({"Support","Dazzle", "Grave"}, "Health", 1, 99, 1)
 
 Support.optionEnabledPurchaseWards = Menu.AddOptionBool({"Support", "Auto purchase", "Wards"}, "Enabled", false)
 Support.optionEnabledPurchaseTome = Menu.AddOptionBool({"Support", "Auto purchase", "TOME"}, "Enabled", false)
@@ -19,6 +21,7 @@ Support.optionEnabledPurchaseTome = Menu.AddOptionBool({"Support", "Auto purchas
 function Support.OnUpdate()
 	if Menu.IsEnabled(Support.optionEnabled) then
 		if Menu.IsEnabled(Support.optionEnabledPurchaseWards) then
+			Wards = NPC.GetItem(myHero, "item_ward_observer");
 			if GameRules.GetGameTime() >= TimerWards*130 then
 				Player.PrepareUnitOrders(myPlayer, Enum.UnitOrder.DOTA_UNIT_ORDER_PURCHASE_ITEM, 42, Vector(0,0,0), 42, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_PASSED_UNIT_ONLY, myHero)
 				TimerWards=TimerWards+1;
@@ -42,7 +45,7 @@ end
 
 function Support.DazzleGrave(HeroesRadius,Grave)
 	for i = 1, #HeroesRadius do
-		if ((Entity.GetMaxHealth(HeroesRadius[i])*0.01)*Menu.GetValue(Support.optionCountEnemyGrave) >= Entity.GetHealth(HeroesRadius[i])) and Entity.GetHeroesInRadius(HeroesRadius[i], 1000, Enum.TeamType.TEAM_ENEMY) then
+		if ((Entity.GetMaxHealth(HeroesRadius[i])*0.01)*Menu.GetValue(Support.optionCountEnemyGrave) >= Entity.GetHealth(HeroesRadius[i])) and Entity.GetHeroesInRadius(HeroesRadius[i], 1000, Enum.TeamType.TEAM_ENEMY) and Ability.IsReady(Grave) then
 			Ability.CastTarget(Grave,HeroesRadius[i],true);
 		end
 	end
